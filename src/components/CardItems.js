20803adbec427fb904a4f87627518e3d0a126d27
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2023-03-28 19:47:35 -0500

    Fixed menu

--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {NavLink} from "react-router-dom";
 import {Card, Row, Col} from "antd";
+import { useMediaQuery } from 'react-responsive';
 import "antd/dist/antd.css";
 import "../pages/Pages.css";
 import "../pages/FrontPage.css";
@@ -8,6 +9,8 @@ import "boxicons";
 // todo: add links, add calendar, add footer, add logo to top
 
 const CardItems = () => {
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+
     const carditems = [
         {
             link: "/board",
@@ -31,8 +34,8 @@ const CardItems = () => {
     return (
         <div class="card">
             <Row gutter={6} id="fancyRow" justify="space-around" align="middle">
-                <Col xs={22} sm={20} md={20} lg={8} xl={8} style={{marginBottom: 10}}>
-                    <img className="arrow-photo" src="/assets/arrow.png"/></Col>
+                {!isTabletOrMobile && <Col xs={22} sm={20} md={20} lg={8} xl={8} style={{marginBottom: 10}}>
+                    <img className="arrow-photo" src="/assets/arrow.png"/></Col>}
                 <Col xs={22} sm={20} md={20} lg={11} xl={15} id="secondCol">
                     <Row gutter={8} id="fancyRow" justify="space-around" align="middle">
                         {carditems.map((carditems) =>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import {Affix, Menu} from "antd";
 import { slide as SlideMenu } from 'react-burger-menu'
 import { useState, useRef } from "react";
+import { useMediaQuery } from 'react-responsive'
 import "./Header.css";
 import "antd/dist/antd.css";
 import "../pages/Pages.css";
@@ -10,6 +11,7 @@ const Header = () => {
     const [isPlaying, setIsPlaying] = useState(false);
     // let song = new Audio("star_spangled_banner.mp3");
     const myRef = useRef();
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
 
     const togglePlay = () => {
         isPlaying ? myRef.current.pause() : myRef.current.play();
@@ -35,17 +37,18 @@ const Header = () => {
     ];
 
     return (
-        <Affix className="menu">
+        <div>
             <audio id="audio" ref={myRef} src="/assets/Ball_so_Hard.mp3" loop="loop" volume="0.5"></audio>
-            <Menu items={menu_items} mode="horizontal" id="webnav"/>
-            <SlideMenu id="mobilemenu">
-                <a href="/board">Meet The Board</a>
-                <a href="/volunteer">Volunteer</a>
-                <a href="/donate">Donate</a>
-                <a href="/">Home</a>
-                <div id="music">{isPlaying ? <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-full'></box-icon> : <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-mute'></box-icon>}</div>
-            </SlideMenu>
-        </Affix>
+            { isTabletOrMobile ? <SlideMenu id="mobilemenu">
+                    <a href="/board">Meet The Board</a>
+                    <a href="/volunteer">Volunteer</a>
+                    <a href="/donate">Donate</a>
+                    <a href="/">Home</a>
+                    <div id="music">{isPlaying ? <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-full'></box-icon> : <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-mute'></box-icon>}</div>
+                </SlideMenu> : <Affix className="menu">
+                    <Menu items={menu_items} mode="horizontal" id="webnav"/>
+                    </Affix>}
+        </div>
     )
 }
 
