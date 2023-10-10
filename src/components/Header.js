492809c86392f7d86721d55d50c4653e02d17c1c
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2023-10-10 13:34:25 -0500

    Fixing more things

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,106 @@
-import {Affix, Menu} from "antd";
+import { Affix, Menu } from "antd";
 import { useState, useRef } from "react";
-import { useMediaQuery } from 'react-responsive'
-import { slide as SlideMenu } from 'react-burger-menu';
-import "boxicons";
+import { useMediaQuery } from "react-responsive";
+import { slide as SlideMenu } from "react-burger-menu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeXmark, faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 import "antd/dist/antd.css";
 import "../pages/Pages.css";
 import "../pages/FrontPage.css";
 
 const Header = () => {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [open, setOpen] = useState(false);
-    // let song = new Audio("star_spangled_banner.mp3");
-    const myRef = useRef();
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [open, setOpen] = useState(false);
+  // let song = new Audio("star_spangled_banner.mp3");
+  const myRef = useRef();
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 
-    const togglePlay = () => {
-        isPlaying ? myRef.current.pause() : myRef.current.play();
-        setIsPlaying(!isPlaying);
-        console.log(isPlaying)
-    };
+  const togglePlay = () => {
+    isPlaying ? myRef.current.pause() : myRef.current.play();
+    setIsPlaying(!isPlaying);
+    console.log(isPlaying);
+  };
 
-    const handleClick = () => {
-        setOpen(!open);
-        console.log(open)
-    };
+  const handleClick = () => {
+    setOpen(!open);
+    console.log(open);
+  };
 
-    const menu_items = [
-        {label: <a href="/board">Meet The Board</a>, key: "bod",},
-        {label: <a href="/volunteer">Volunteer</a>, key: "volunteer"}, // which is required
-        {label: <a href="/donate">Donate</a>, key: "donate"}, // remember to pass the key prop
-        {label: <a href="/">Home</a>, key: "home"},
-        {label: (<div id="music">{isPlaying ? <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-full'></box-icon> : <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-mute'></box-icon>}</div>), key: "music"},
-        {
-            label: (
-                <a href="/" id="eagle_logo">
-                    <img className="eagle-photo" src="/assets/basketball-icon-header.png" alt="logo" height="50em"/>
-                </a>
-            ),
-            key: "logo",
-            id: "eagle_logo",
-        },
-    ];
-
-    return (
-        <div>
-            <audio id="audio" ref={myRef} src="/assets/Ball_so_Hard.mp3" loop="loop" volume="0.5"></audio>
-            {isTabletOrMobile ? <SlideMenu id="mobilemenu">
-                    <a href="/board">Meet The Board</a>
-                    <a href="/volunteer">Volunteer</a>
-                    <a href="/donate">Donate</a>
-                    <a href="/">Home</a>
-                    <div id="music">{isPlaying ? <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-full'></box-icon> : <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-mute'></box-icon>}</div>
-                </SlideMenu> : <Affix className="menu">
-                    <Menu items={menu_items} mode="horizontal" id="webnav"/>
-                    </Affix>}
+  const menu_items = [
+    { label: <a href="/board">Meet The Board</a>, key: "bod" },
+    { label: <a href="/volunteer">Volunteer</a>, key: "volunteer" }, // which is required
+    { label: <a href="/donate">Donate</a>, key: "donate" }, // remember to pass the key prop
+    { label: <a href="/">Home</a>, key: "home" },
+    // {label: (<div id="music">{isPlaying ? <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-full'></box-icon> : <box-icon color="white" class="small-icon" onClick={togglePlay} name='volume-mute'></box-icon>}</div>), key: "music"},
+    {
+      label: (
+        <div id="music">
+          {isPlaying ? (
+            <FontAwesomeIcon icon={faVolumeHigh} onClick={togglePlay} />
+          ) : (
+            <FontAwesomeIcon icon={faVolumeXmark} onClick={togglePlay} />
+          )}
         </div>
-    )
-}
+      ),
+      key: "music",
+    },
+    {
+      label: (
+        <a href="/" id="eagle_logo">
+          <img
+            className="eagle-photo"
+            src="/assets/basketball-icon-header.png"
+            alt="logo"
+            height="50em"
+          />
+        </a>
+      ),
+      key: "logo",
+      id: "eagle_logo",
+    },
+  ];
+
+  return (
+    <div>
+      <audio
+        id="audio"
+        ref={myRef}
+        src="/assets/Ball_so_Hard.mp3"
+        loop="loop"
+        volume="0.5"
+      ></audio>
+      {isTabletOrMobile ? (
+        <SlideMenu id="mobilemenu">
+          <a href="/board">Meet The Board</a>
+          <a href="/volunteer">Volunteer</a>
+          <a href="/donate">Donate</a>
+          <a href="/">Home</a>
+          <div id="music">
+            {isPlaying ? (
+              <box-icon
+                color="white"
+                class="small-icon"
+                onClick={togglePlay}
+                name="volume-full"
+              ></box-icon>
+            ) : (
+              <box-icon
+                color="white"
+                class="small-icon"
+                onClick={togglePlay}
+                name="volume-mute"
+              ></box-icon>
+            )}
+          </div>
+        </SlideMenu>
+      ) : (
+        <Affix className="menu">
+          <Menu items={menu_items} mode="horizontal" id="webnav" />
+        </Affix>
+      )}
+    </div>
+  );
+};
 
 export default Header;
--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -2,14 +2,11 @@ import "antd/dist/antd.css";
 import "./FrontPage.css";
 import "./Pages.css";
 import "boxicons";
-import FullCalendar from "@fullcalendar/react";
-import googleCalendarPlugin from "@fullcalendar/google-calendar";
-import dayGridPlugin from "@fullcalendar/daygrid";
 import CardItems from "../components/CardItems";
 import Gallery from "./Gallery";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 // todo: add links, add calendar, add footer, add logo to top
 
 let API_KEY = process.env.REACT_APP_API_KEY;
@@ -17,85 +14,123 @@ let CALENDAR_ID = process.env.REACT_APP_CALENDAR_ID;
 
 //adds 'loaded' classname to a element when website loaded
 window.onload = function () {
-    document.body.className += "loaded";
-}
+  document.body.className += "loaded";
+};
 
 const FrontPage = () => {
-    const [events, setEvents] = useState([]);
-    useEffect(() => {
-        fetch(`https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}&timeMin=${new Date().toISOString()}&showDeleted=false&singleEvents=true&maxResults=10&orderBy=startTime`)
-            .then((response) => response.json())
-            .then((data) => setEvents(data.items.slice(0, 15)));
-    }, []);
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}&timeMin=${new Date().toISOString()}&showDeleted=false&singleEvents=true&maxResults=10&orderBy=startTime`
+    )
+      .then((response) => response.json())
+      .then((data) => setEvents(data.items.slice(0, 15)));
+  }, []);
 
-    function fixDate(date) {
-        let d = new Date(date);
-        let time = d.toLocaleTimeString();
-        return `${d.toLocaleDateString()} \n ${time.substr(0, time.lastIndexOf(":")) + time.substr(time.lastIndexOf(" "), time.length)}`;
-    }
+  function fixDate(date) {
+    let d = new Date(date);
+    let time = d.toLocaleTimeString();
+    return `${d.toLocaleDateString()} \n ${
+      time.substr(0, time.lastIndexOf(":")) +
+      time.substr(time.lastIndexOf(" "), time.length)
+    }`;
+  }
 
-    return (
-        <div>
-            <Header/>
-            <div className="wrapper">
-                <div className="title_screen">
-                        <img className="full_logo" src="/assets/Full_logo_cropped.png"/>
-                    <div className="title">
-                        <div className="banner"></div>
-                        <div class="title-text">
-                            <h3>Demanding Excellence in Everything We Do</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className="Border">
-                    <div className="border_box"></div>
-                </div>
-                <div class="bodyParagraph">
-                    <h1 class="hidden">What We Do</h1>
-                    <br></br>
-                    <img className="photo-collage" src="/assets/photo_collage-two.png"/>
-                    <p class="hidden">
-                        The Stuttgart Eagles Basketball Club (SEBC) is made up of Department
-                        of Defense dependents and local youth within the Stuttgart, Germany
-                        community. We are going into our tenth year in 2023 as an organization.
-                        We operate as a non-profit organization with 100% volunteers.
-                        SEBC travels locally and throughout Europe dominating tournaments during
-                        basketball season and competing against over 30 International Clubs annually.
-                        We represent the USA in a positive manner, both athletically and in sportsmanship.
-                        SEBC bridges a gap in the overseas military community for school-age children
-                        who are not afforded the experience of competitive Amateur Athletic Union (AAU)
-                        and middle school sports during their adolescent years due to their parents
-                        serving our Nation abroad. We are also excited to start an Eagles Cheer
-                        program this year! In addition to basketball and cheer, we require our players
-                        maintain a high grade point average and have no disciplinary problems.
-                        Our goal is to not only give our kids a fighting chance athletically with their
-                        stateside counterparts, but also to build kids with character.{" "}
-                    </p>
-                </div>
-                <div className="cardgrid">
-                    <div class="cards">
-                        <CardItems/>
-                    </div>
-                </div>
-                <br></br><br></br>
-                <div id="eventsList">
-                    <div className="bodyParagraph"><h2>Upcoming Events</h2></div>
-                    <ul class="eventsList">
-                        {events.map((event) => (
-                            <li key={event.id} class="event" style={{whiteSpace: "pre-line"}}>
-                                <p class="date">{fixDate(event.start.dateTime)}</p>
-                                <div class="info"><h3>{event.summary}</h3>
-                                    {event.location && <p>{event.location}</p>}</div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="bodyParagraph"><h2>Gallery</h2></div>
-                <Gallery/>
-                <Footer/>
+  return (
+    <div className="front-page">
+      <img
+        src="/assets/Eagles Beach Banner.png"
+        className="leftbanner"
+        alt="Beach banner"
+      />
+      <img
+        src="/assets/Eagles Beach Banner.png"
+        className="rightbanner"
+        alt="Beach banner"
+      />
+      <div className="front-page-inner">
+        <Header />
+        <div className="wrapper">
+          <div className="title_screen">
+            <img
+              className="full_logo"
+              src="/assets/Full_logo_cropped.png"
+              alt="Logo"
+            />
+            <div className="title">
+              <div className="banner"></div>
+              <div class="title-text">
+                <h3>Demanding Excellence in Everything We Do</h3>
+              </div>
             </div>
+          </div>
+          <div className="Border">
+            <div className="border_box"></div>
+          </div>
+          <div class="bodyParagraph">
+            <h1 class="hidden">What We Do</h1>
+            <br></br>
+            <img
+              className="photo-collage"
+              src="/assets/photo_collage-two.png"
+            />
+            <p class="hidden">
+              The Stuttgart Eagles Basketball Club (SEBC) is made up of
+              Department of Defense dependents and local youth within the
+              Stuttgart, Germany community. We are going into our tenth year in
+              2023 as an organization. We operate as a non-profit organization
+              with 100% volunteers. SEBC travels locally and throughout Europe
+              dominating tournaments during basketball season and competing
+              against over 30 International Clubs annually. We represent the USA
+              in a positive manner, both athletically and in sportsmanship. SEBC
+              bridges a gap in the overseas military community for school-age
+              children who are not afforded the experience of competitive
+              Amateur Athletic Union (AAU) and middle school sports during their
+              adolescent years due to their parents serving our Nation abroad.
+              We are also excited to start an Eagles Cheer program this year! In
+              addition to basketball and cheer, we require our players maintain
+              a high grade point average and have no disciplinary problems. Our
+              goal is to not only give our kids a fighting chance athletically
+              with their stateside counterparts, but also to build kids with
+              character.{" "}
+            </p>
+          </div>
+          <div className="cardgrid">
+            <div class="cards">
+              <CardItems />
+            </div>
+          </div>
+          <br></br>
+          <br></br>
+          <div id="eventsList">
+            <div className="bodyParagraph">
+              <h2>Upcoming Events</h2>
+            </div>
+            <ul class="eventsList">
+              {events.map((event) => (
+                <li
+                  key={event.id}
+                  class="event"
+                  style={{ whiteSpace: "pre-line" }}
+                >
+                  <p class="date">{fixDate(event.start.dateTime)}</p>
+                  <div class="info">
+                    <h3>{event.summary}</h3>
+                    {event.location && <p>{event.location}</p>}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="bodyParagraph">
+            <h2>Gallery</h2>
+          </div>
+          <Gallery />
+          <Footer />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FrontPage;
