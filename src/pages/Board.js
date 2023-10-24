2f495e6fa29bc48535b9c6e4598092795243c17e
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2023-10-24 18:40:21 -0500

    Small change part two

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,79 +1,92 @@
 import "./Board.css";
 import "./Pages.css";
-import {Row, Col} from "antd";
+import { Row, Col } from "antd";
 import "antd/dist/antd.css";
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const people = [
-    {
-        name: "Huntley Guthrie",
-        role: "Director",
-        image: "/assets/huntley.jpg",
-    },
-    {
-        name: "Ryan Roseberry",
-        role: "Vice Director",
-        image: "/assets/ryan.jpg",
-    },
-    {
-        name: "Jessica Brown",
-        role: "Secretary",
-        image: "/assets/jessica.jpg",
-    },
-    {
-        name: "Shawn Gilliland",
-        role: "Treasurer",
-        image: "/assets/shawn.jpg",
-    },
-    {
-        name: "Nadia Pontif",
-        role: "Social Media Coordinator",
-        image: "/assets/currently_unnamed.jpg",
-    },
-    {
-        name: "Stephanie Lee",
-        role: "Fundraising Coordinator",
-        image: "/assets/stephanie.jpg",
-    },
-    {
-        name: "Mike DeRosier",
-        role: "Event Coordinator",
-        image: "/assets/derosier.jpg",
-    },
-    {
-        name: "Mike Stevenson",
-        role: "Event Coordinator",
-        image: "/assets/stevenson.jpg",
-    },
+  {
+    name: "Huntley Guthrie",
+    role: "Director",
+    image: "/assets/huntley.jpg",
+  },
+  {
+    name: "Ryan Roseberry",
+    role: "Vice Director",
+    image: "/assets/ryan.jpg",
+  },
+  {
+    name: "Jessica Brown",
+    role: "Secretary",
+    image: "/assets/jessica.jpg",
+  },
+  {
+    name: "Shawn Gilliland",
+    role: "Treasurer",
+    image: "/assets/shawn.jpg",
+  },
+  {
+    name: "Nadia Pontif",
+    role: "Social Media Coordinator",
+    image: "/assets/currently_unnamed.jpg",
+  },
+  {
+    name: "Stephanie Lee",
+    role: "Fundraising Coordinator",
+    image: "/assets/stephanie.jpg",
+  },
+  {
+    name: "Mike DeRosier",
+    role: "Event Coordinator",
+    image: "/assets/derosier.jpg",
+  },
+  {
+    name: "Mike Stevenson",
+    role: "Event Coordinator",
+    image: "/assets/stevenson.jpg",
+  },
 ];
 
 const Board = () => {
-    return (
-        <div>
-            <Header/>
-            <div className="wrapper">
-            <div className="bodyParagraph">
-                <h1>Meet the Board</h1>
-                <br></br>
-                <Row gutter={20}>
-                    {people.map((board) => (
-                        <Col xs={24} sm={24} md={12} lg={12} xl={8}>
-                            <div className="boardcard">
-                                <img className="boardpic" src={board.image}/>
-                                <img className="basketball-icon" src="/assets/basketball-icon-red.png" />
-                                <h2>{board.name}</h2>
-                                <h3>{board.role}</h3>
-                                <br></br>
-                            </div>
-                        </Col>
-                    ))}
-                </Row>
-            </div>
-            <Footer/>
-            </div>
+  return (
+    <div className="main">
+      <img
+        src="/assets/Eagles_Beach_Banner.jpg"
+        className="leftbanner"
+        alt="Beach banner"
+      />
+      <img
+        src="/assets/Eagles_Beach_Banner.jpg"
+        className="rightbanner"
+        alt="Beach banner"
+      />
+      <Header />
+      <div className="wrapper">
+        <div className="bodyParagraph">
+          <h1>Meet the Board</h1>
+          <br></br>
+          <Row gutter={20}>
+            {people.map((board) => (
+              <Col xs={24} sm={24} md={12} lg={12} xl={8}>
+                <div className="boardcard">
+                  <img
+                    className="boardpic"
+                    alt="board member"
+                    src={board.image}
+                  />
+                  <h2>{board.name}</h2>
+                  <h3>{board.role}</h3>
+                  <br></br>
+                </div>
+              </Col>
+            ))}
+          </Row>
         </div>
-    );
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Board;
--- src/pages/Volunteer.js
+++ src/pages/Volunteer.js
@@ -1,69 +1,91 @@
 import "./Pages.css";
 import "./Volunteer.css";
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Volunteer = () => {
-    return (
-        <div>
-            <Header/>
-            <div className="wrapper">
+  return (
+    <div className="main">
+      <img
+        src="/assets/Eagles_Beach_Banner.jpg"
+        className="leftbanner"
+        alt="Beach banner"
+      />
+      <img
+        src="/assets/Eagles_Beach_Banner.jpg"
+        className="rightbanner"
+        alt="Beach banner"
+      />
+      <Header />
+      <div className="wrapper">
+        <div className="bodyParagraph">
+          <h1>Volunteer Work</h1>
+          <p>
+            Our team relies on the compassionate work of our volunteers, and
+            numerous volunteer position are open.
+          </p>
+          <p>
+            if you wish to volunteer, please reach out to us: <br></br>
+            <b>stuttgarteaglesbasketball@googlemail.com</b>
+          </p>
+          <br></br>
 
-                <div className="bodyParagraph">
-                    <h1>Volunteer Work</h1>
-                    <p>Our team relies on the compassionate work of our volunteers,
-                        and numerous volunteer position are open.</p>
-                    <p>if you wish to volunteer, please reach out to us: <br></br>
-                        <b>stuttgarteaglesbasketball@googlemail.com</b>
-                    </p>
-                    <br></br>
-
-                    <div className="vol-grid">
-                        <div className="right">
-                            <h2>Coaching</h2>
-                            <p>
-                                The success of many youth sports leagues is rooted in its volunteer coaches.
-                                But for our organization and transitional community recruitment can be a seasonal
-                                and recurring challenge. If you have a love of basketball and cheer and what to make
-                                a difference, reach out to us.
-                            </p>
-                        </div>
-                        <div>
-                            <img className="coachingpic" src="assets/coaching_cropped.webp" />
-                        </div>
-                        <div className="left">
-                            <h2>Board Members</h2>
-                            <p>
-                                Serving on the board is a wonderful way to support our club and mission.
-                                It also can be a powerful way to build your own skills and expertise.
-                                Individuals who serve on a board have the opportunity to develop and grow as leaders,
-                                cultivate new skill sets, and expand their network of peers, professionals,
-                                community leaders, and community thought leaders. Are you ready to help?
-                            </p>
-                        </div>
-                        <div>
-                            <img className="boardmempic" src="/assets/teamwork-cropped.webp"/>
-                        </div>
-                        <div className="right">
-                            <h2>Fundraising</h2>
-                            <p>
-                                Want to help but don't have time to commit to a full time board position?
-                                Consider leading a fundraiser event. This can be anything from a food or bake sale
-                                to a gala or more. The ideas are endless. Join the SEBC team and help inspire others
-                                on why the funds are being raised and the importance the our club in the community.
-                            </p>
-                        </div>
-                        <div>
-                            <img className="fundraisepic" src="/assets/younger_players.webp"/>
-                        </div>
-                    </div>
-                </div>
-                <br></br>
-                <Footer/>
+          <div className="vol-grid">
+            <div className="right">
+              <h2>Coaching</h2>
+              <p>
+                The success of many youth sports leagues is rooted in its
+                volunteer coaches. But for our organization and transitional
+                community recruitment can be a seasonal and recurring challenge.
+                If you have a love of basketball and cheer and what to make a
+                difference, reach out to us.
+              </p>
             </div>
-
+            <div>
+              <img className="coachingpic" src="assets/coaching_cropped.webp" />
+            </div>
+            <div>
+              <img
+                className="boardmempic"
+                src="/assets/teamwork-cropped.webp"
+              />
+            </div>
+            <div className="left">
+              <h2>Board Members</h2>
+              <p>
+                Serving on the board is a wonderful way to support our club and
+                mission. It also can be a powerful way to build your own skills
+                and expertise. Individuals who serve on a board have the
+                opportunity to develop and grow as leaders, cultivate new skill
+                sets, and expand their network of peers, professionals,
+                community leaders, and community thought leaders. Are you ready
+                to help?
+              </p>
+            </div>
+            <div className="right">
+              <h2>Fundraising</h2>
+              <p>
+                Want to help but don't have time to commit to a full time board
+                position? Consider leading a fundraiser event. This can be
+                anything from a food or bake sale to a gala or more. The ideas
+                are endless. Join the SEBC team and help inspire others on why
+                the funds are being raised and the importance the our club in
+                the community.
+              </p>
+            </div>
+            <div>
+              <img
+                className="fundraisepic"
+                src="/assets/younger_players.webp"
+              />
+            </div>
+          </div>
         </div>
-    )
-}
+        <br></br>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Volunteer;
